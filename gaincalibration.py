9cd363ecd9834f6e2094093841a048c18424cd64
Reed Cohen <39147325+atardigradestory@users.noreply.github.com>
2020-02-18 15:54:37 -0500

    Add files via upload
    Some additional steps and plotting code

--- gaincalibration.py
+++ gaincalibration.py
@@ -5,12 +5,6 @@ Created on Tue Feb 11 10:27:00 2020
 @author: lab-341
 """
 
-from scipy import signal  # needs to be here otherwise sp.signal won't be recognized
-from tkinter import messagebox
-from tkFileDialog import askdirectory, askopenfilename
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-import queue
-import threading
 import datetime
 from numpy.polynomial.polynomial import Polynomial
 import fnmatch
@@ -18,13 +12,8 @@ import pandas as pd
 import scipy as sp
 import numpy as np
 import zipfile
-from uuid import uuid4
-import re
 import os
-import ttk
-import Tkinter as tk
 from matplotlib import pyplot as plt
-from matplotlib.figure import Figure
 from matplotlib import style
 import matplotlib
 matplotlib.use("TkAgg", warn=False)
@@ -40,6 +29,30 @@ def df_window(df):
     with NamedTemporaryFile(delete = False,suffix = '.html') as f:
         df.to_html(f)
     webbrowser.open(f.name)
+
+def blinfit(x,y,err):
+    x = sp.array(x)
+    x = x.astype(float)
+    y = sp.array(y)
+    y = y.astype(float)
+    err = sp.array(err)
+    w = err**(-2)
+    
+    
+    wx = w*x
+    wx2 = w*(x**2)
+    wy = w*y
+    wxy = (w*x)*y
+    
+    D = w.sum()*wx2.sum() - wx.sum()**2
+    c = (wx2.sum()*wy.sum() - wx.sum()*wxy.sum())/D
+    m = (w.sum()*wxy.sum() - wx.sum()*wy.sum())/D
+    
+    ac = np.sqrt(wx2.sum()/D)
+    am = np.sqrt(w.sum()/D)
+    
+    return (c,m,ac,am)
+
 
 def rms(array):
     avg = 0
@@ -91,20 +104,30 @@ def process_waveforms(rundir):
     
     for wf in wfs:
         x, y, time = get_wf(wf, rundir)
-        output = max(y)
+        if x == None:
+            return None
+        #get baseline
+        peak = y.index(max(y))
+        noisex = np.array(x)
+        ind = len(noisex[noisex < 2])
+        bl = y[:ind]
+        avg = np.mean(bl)
+        #-----------#
+        output = max(y) - avg
         yl.append(output)
-    
     yl = sp.array(yl)
     avg = yl.mean()
     err = rms(yl - yl.mean())
     return (avg,err,wfcount)
+
+
 
 folder = 'D:\Xe\ShAmpCalibrationsNov2019'
 dates = os.listdir(folder)
 
 DF = pd.DataFrame(data=None, columns=['date','input','freq','gain','peak_avg','peak_std','wfcount'])
 
-for date in dates:
+for date in dates[1:-2]:
     print 'On date: ', date
     ext = folder + '\\'+ date + '\Waveforms\\'
     runs = os.listdir(ext)
@@ -127,3 +150,87 @@ for date in dates:
         series = pd.Series(data)
         DF = DF.append(series,ignore_index=True)
         print 'Run Finished'
+        
+#solution for the weird ass formatting issue
+DF = DF.replace({'14000000000000002':'14mV',
+                  '21999999999999996':'22mV',
+                  '7000000000000001':'7cV',
+                  '41499999999999992':'415mV'})
+    
+#a solution for the horrible format of '.1V' = '1dV'
+def quickreplace(string):
+    mag = string[-2]
+    try:
+        int(mag)
+        return float(string[:-1])
+    except ValueError:
+        pass
+    num = float(string[:-2])
+    if mag == 'd':
+        num = num*0.1
+    if mag == 'c':
+        num = num*0.01
+    if mag == 'm':
+        num = num*0.001
+    return num
+
+DF.input = DF.input.apply(quickreplace)
+##
+'''
+gains= [0,1,10,11]
+
+for gain in gains:
+    plt.figure()
+    df = DF[DF.gain == gain]
+    df = df.sort_values(by=['input'])
+    handles = []
+    labels = []
+    for date in set(df.date):
+        data = df[df.date == date]
+        x = data.input
+        y = data.peak_avg
+        yerr = data.peak_std
+        handle = plt.scatter(x,y)
+        handles.append(handle)
+        labels.append(date)
+    
+    #now lin fit
+    x = df.input
+    y = df.peak_avg
+    yerr = df.peak_std
+    c,m,ac,am = blinfit(x,y,yerr)
+    xax = np.linspace(0,max(x))
+    handle = plt.plot(xax,m*xax+c)
+    handles.append(handle[0])
+    labels.append(u'Slope: {:0.2} \u00B1 {:0.1} , \u00B1 Offset: {:0.2} \u00B1 {:0.1}'.format(m,am,c,ac))
+    plt.title('Gain Setting {}'.format(gain))
+    plt.legend(handles,labels)
+    plt.savefig('Gain{}.png'.format(gain))
+    plt.xlabel('Input (V)')
+    plt.ylabel('Output (V)')
+    
+    
+#method for getting a sample of wf plots
+for date in dates[1:-2]:
+    #directory stuff
+    ext = folder + '\\'+ date + '\Waveforms\\'
+    runs = os.listdir(ext)
+    run = runs[0]
+    #get params
+    hold = run.split('_')
+    gain = hold[3]
+    Vin = hold[5]
+    #more dir stuff
+    rundir = ext + '\\' + run
+    wfs = fnmatch.filter(os.listdir(rundir),'w*')
+    wf = wfs[0]
+    #get data and plot
+    x,y,time = get_wf(wf,rundir)
+    plt.figure()
+    plt.plot(x,y)
+    plt.title('Date: {} , Gain: {}, Input: {}'.format(date,gain,Vin))
+    plt.xlabel('Time (us)')
+    plt.ylabel('Output (V)')
+    plt.savefig('ShaperWF{}.png'.format(date))
+#-------------------------------------#
+ '''
